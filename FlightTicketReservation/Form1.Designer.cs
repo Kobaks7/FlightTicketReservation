a40b74af72cad4f66851400253f8ea254262170b
Kadir Kobak <56497246+Kobaks7@users.noreply.github.com>
2024-01-28 01:47:01 +0300

    Proje dosyası ekle.

---
+++ FlightTicketReservation/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿
+namespace FlightTicketReservation
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.startingPoint = new System.Windows.Forms.ComboBox();
+            this.endPoint = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.date = new System.Windows.Forms.DateTimePicker();
+            this.time = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.passengerName = new System.Windows.Forms.TextBox();
+            this.passengerTCNo = new System.Windows.Forms.MaskedTextBox();
+            this.passengerNumber = new System.Windows.Forms.MaskedTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.informationDisplay = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.changeLocationBtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.time);
+            this.groupBox1.Controls.Add(this.date);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.endPoint);
+            this.groupBox1.Controls.Add(this.startingPoint);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(28, 121);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(417, 291);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Uçuş Bilgileri";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.saveBtn);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.passengerNumber);
+            this.groupBox2.Controls.Add(this.passengerTCNo);
+            this.groupBox2.Controls.Add(this.passengerName);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.Location = new System.Drawing.Point(483, 121);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(417, 291);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Yolcu Bilgileri";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(31, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kalkış Noktası:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(37, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Varış Noktası:";
+            // 
+            // startingPoint
+            // 
+            this.startingPoint.FormattingEnabled = true;
+            this.startingPoint.Items.AddRange(new object[] {
+            "İstanbul",
+            "İzmir ",
+            "Ankara",
+            "Antalya ",
+            "Bursa"});
+            this.startingPoint.Location = new System.Drawing.Point(176, 75);
+            this.startingPoint.Name = "startingPoint";
+            this.startingPoint.Size = new System.Drawing.Size(181, 32);
+            this.startingPoint.TabIndex = 2;
+            // 
+            // endPoint
+            // 
+            this.endPoint.FormattingEnabled = true;
+            this.endPoint.Items.AddRange(new object[] {
+            "İstanbul",
+            "İzmir ",
+            "Ankara",
+            "Antalya ",
+            "Bursa"});
+            this.endPoint.Location = new System.Drawing.Point(176, 112);
+            this.endPoint.Name = "endPoint";
+            this.endPoint.Size = new System.Drawing.Size(181, 32);
+            this.endPoint.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(22, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Yolculuk Tarihi:";
+            // 
+            // date
+            // 
+            this.date.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.date.Location = new System.Drawing.Point(176, 152);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(181, 28);
+            this.date.TabIndex = 5;
+            // 
+            // time
+            // 
+            this.time.Location = new System.Drawing.Point(176, 187);
+            this.time.Mask = "00:00";
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(181, 30);
+            this.time.TabIndex = 6;
+            this.time.ValidatingType = typeof(System.DateTime);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(31, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Yolculuk Saati:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(6, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(148, 23);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Yolcu Ad Soyad:";
+            // 
+            // passengerName
+            // 
+            this.passengerName.Location = new System.Drawing.Point(160, 77);
+            this.passengerName.Name = "passengerName";
+            this.passengerName.Size = new System.Drawing.Size(181, 30);
+            this.passengerName.TabIndex = 8;
+            // 
+            // passengerTCNo
+            // 
+            this.passengerTCNo.Location = new System.Drawing.Point(160, 114);
+            this.passengerTCNo.Mask = "00000000000";
+            this.passengerTCNo.Name = "passengerTCNo";
+            this.passengerTCNo.Size = new System.Drawing.Size(181, 30);
+            this.passengerTCNo.TabIndex = 9;
+            this.passengerTCNo.ValidatingType = typeof(int);
+            // 
+            // passengerNumber
+            // 
+            this.passengerNumber.Location = new System.Drawing.Point(160, 153);
+            this.passengerNumber.Mask = "(999) 000-0000";
+            this.passengerNumber.Name = "passengerNumber";
+            this.passengerNumber.Size = new System.Drawing.Size(181, 30);
+            this.passengerNumber.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(22, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(132, 23);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "TC Kimlik No:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Georgia", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(46, 160);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(108, 23);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Telefon No:";
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.saveBtn.Location = new System.Drawing.Point(160, 203);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(181, 34);
+            this.saveBtn.TabIndex = 2;
+            this.saveBtn.Text = "Kaydet";
+            this.saveBtn.UseVisualStyleBackColor = false;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // informationDisplay
+            // 
+            this.informationDisplay.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.informationDisplay.FormattingEnabled = true;
+            this.informationDisplay.HorizontalScrollbar = true;
+            this.informationDisplay.ItemHeight = 24;
+            this.informationDisplay.Location = new System.Drawing.Point(28, 456);
+            this.informationDisplay.Name = "informationDisplay";
+            this.informationDisplay.Size = new System.Drawing.Size(872, 124);
+            this.informationDisplay.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightCoral;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Location = new System.Drawing.Point(28, 15);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(872, 84);
+            this.panel1.TabIndex = 3;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe Print", 19.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(67, 11);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(325, 61);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "XYZ Hava Yolları";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(545, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(206, 84);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // changeLocationBtn
+            // 
+            this.changeLocationBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.changeLocationBtn.Location = new System.Drawing.Point(391, 196);
+            this.changeLocationBtn.Name = "changeLocationBtn";
+            this.changeLocationBtn.Size = new System.Drawing.Size(48, 69);
+            this.changeLocationBtn.TabIndex = 8;
+            this.changeLocationBtn.Text = "<>";
+            this.changeLocationBtn.UseVisualStyleBackColor = false;
+            this.changeLocationBtn.Click += new System.EventHandler(this.changeLocationBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 609);
+            this.Controls.Add(this.changeLocationBtn);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.informationDisplay);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Uçak Bileti Rezervasyon Sistemi";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox endPoint;
+        private System.Windows.Forms.ComboBox startingPoint;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox time;
+        private System.Windows.Forms.DateTimePicker date;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox passengerNumber;
+        private System.Windows.Forms.MaskedTextBox passengerTCNo;
+        private System.Windows.Forms.TextBox passengerName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.ListBox informationDisplay;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button changeLocationBtn;
+    }
+}
+
---
+++ FlightTicketReservation/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlightTicketReservation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            if (startingPoint.Text!=endPoint.Text)
+            {
+                informationDisplay.Items.Add("Rota: "+startingPoint.Text+" - "+endPoint.Text+" Tarih: "+date.Text
+                        +" Saat: "+time.Text+" Yolcu Ad Soyad: "+passengerName.Text+" TC Kimlik No: "+passengerTCNo.Text
+                        +" Telefon No: "+passengerNumber.Text);
+
+                MessageBox.Show("Yolcu kaydı yapıldı");
+            }
+            else
+            {
+                MessageBox.Show("Kalkış noktası ve varış noktası aynı olamaz!");
+            }
+            
+        }
+
+        private void changeLocationBtn_Click(object sender, EventArgs e)
+        {
+            string startPos = startingPoint.Text;
+            string endPos = endPoint.Text;
+
+            startingPoint.Text = endPos;
+            endPoint.Text = startPos;
+        }
+    }
+}
